2910f137cf3ebbaa36c163882fd355c40e8942cc
wddyousuf <saikatyousuf@gmail.com>
2022-06-14 18:31:37 +0600

    facade

--- src/Facades/PaperflyCourier.php
+++ src/Facades/PaperflyCourier.php
@@ -2,6 +2,15 @@
 namespace Wddyousuf\Paperfly\Facades;
 use Illuminate\Support\Facades\Facade;
 
+
+/**
+ * @method static Wddyousuf\Paperfly\Provider\PaperflyCourier sendRequest(array $orderInformation=[])
+ * @method static Wddyousuf\Paperfly\Provider\PaperflyCourier trackOrder(array $orderId)
+ * @method static Wddyousuf\Paperfly\Provider\PaperflyCourier courierInvoice(array $orderId)
+ * @method static Wddyousuf\Paperfly\Provider\PaperflyCourier CourierOrderCancel(array $orderId)
+ *
+ * @see \Wddyousuf\Paperfly\Provider\PaperflyCourier
+ */
 class PaperflyCourier extends Facade
 {
     protected static function getFacadeAccessor()
